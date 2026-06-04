d84ba95bb3832440740a37acc1a5fe527ed51d79
Galaceon <antoniogarcialtr@gmail.com>
2026-06-04 20:10:54 +0200

    validation with Valivod before send data to REST API

--- client/src/services/ProductService.ts
+++ client/src/services/ProductService.ts
@@ -1,9 +1,22 @@
-
+import { DraftProductSchema } from "../types"
+import { safeParse } from "valibot"
 
 type ProductData = {
     [k: string]: FormDataEntryValue;
 }
 
 export async function addProduct(data : ProductData) {
-    console.log(data)
+    try {
+        const result = safeParse(DraftProductSchema, {
+            name: data.name,
+            price: +data.price
+        })
+        if(result.success) {
+
+        } else {
+            throw new Error('Datos no validos')
+        }
+    } catch (error) {
+        console.log(error)
+    }
 }
