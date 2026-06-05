afe97fc213eb10e931ac91550e9f5cf4d2b69e7a
Galaceon <antoniogarcialtr@gmail.com>
2026-06-05 23:11:19 +0200

    enabling CORS in the backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import router from './router'
+import cors, { CorsOptions }  from 'cors'
 import db from './config/db'
 import colors from 'colors'
 import swaggerUI from 'swagger-ui-express'
@@ -19,6 +20,18 @@ export async function connectDB() {
 
 //Instancia de Express
 const server = express()
+
+// Permitir conexiones
+const corsOptions : CorsOptions = {
+    origin: function(origin, callback) {
+        if(origin === process.env.FRONTEND_URL) {
+            callback(null, true)
+        } else {
+            callback(new Error('Error de CORS'))
+        }
+    }
+}
+server.use(cors(corsOptions))
 
 // Leer datos de formularios
 server.use(express.json())
